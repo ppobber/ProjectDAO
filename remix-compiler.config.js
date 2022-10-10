044778e3cbb5edee665f21067dc773075210e798
Jarturong <jthi3922@uni.sydney.edu.au>
2022-10-11 09:45:00 +1100

    update project record

--- remix-compiler.config.js
+++ remix-compiler.config.js
@@ -1,7 +1,7 @@
 module.exports = {
               compilers: {
                 solc: {
-                  version: '0.8.7',
+                  version: '0.8.6',
                   settings: {
                     optimizer: {
                       enabled: false,
