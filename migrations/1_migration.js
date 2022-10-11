92f1ef42ff1c37973cc66dc4f21c997abc9ed05a
Jarturong <jthi3922@uni.sydney.edu.au>
2022-10-11 13:53:35 +1100

    update migration and ignore

--- migrations/1_migration.js
+++ migrations/1_migration.js
@@ -2,20 +2,20 @@
 
 const DaoAcc = artifacts.require('./organization/DaoAccessControl');
 const DaoRecord = artifacts.require('./organization/DaoRecord');
-const DaoToken = artifacts.require('./organization/DaoToken');
+//const DaoToken = artifacts.require('./organization/DaoToken');
 const ProAcc = artifacts.require('./project-name1/ProjectAccessControl');
-// const ProRecord = artifacts.require('/project-name1/ProjectRecord');
-// const ProToken = artifacts.require('/project-name1/ProjectToken');
+const ProRecord = artifacts.require('./project-name1/ProjectRecord');
+// const ProToken = artifacts.require('./project-name1/ProjectToken');
 
 module.exports = async function (deployer) {
   // Command Truffle to deploy the Smart Contract
   await deployer.deploy(DaoAcc);
   await deployer.deploy(DaoRecord, DaoAcc.address);
-  await deployer.deploy(DaoToken, DaoAcc.address);
+  //await deployer.deploy(DaoToken, DaoAcc.address);
   //const token = await OrgToken.deployed();
   //await deployer.deploy(MyOrg, OrgToken.address);
   //const org = MyOrg.deployed();
-  await deployer.deploy(ProAcc, DaoAcc.address);
-  // await deployer.deploy(ProRecord, ProAcc);
+  await deployer.deploy(ProAcc, DaoAcc.address, 'Zoe', 'Zoe@email');
+  await deployer.deploy(ProRecord, ProAcc.address);
   // await deployer.deploy(ProToken, ProAcc, PROJECTTOKEN, PTK);
 };
