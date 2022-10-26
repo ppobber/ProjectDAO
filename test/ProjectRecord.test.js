586cbd22673f6876c0ff51752571787ffd364546
Jarturong <jthi3922@uni.sydney.edu.au>
2022-10-26 15:48:22 +1100

    update test script for all project rec function

--- test/ProjectRecord.test.js
+++ test/ProjectRecord.test.js
@@ -25,4 +25,87 @@ contract('ProjectRecord', function (accounts) {
       'Record project Objectivefailed.'
     );
   });
+
+  it('Record project Budget', async () => {
+    const projectRecord = await ProjectRecord.deployed();
+    console.log('record file projectBudget.json to contract...');
+    await projectRecord.setProjectBudget('projectBudget.json');
+    const projectBudget = await projectRecord.getProjectBudget();
+    console.log('projectBudget: ', projectBudget);
+    assert.equal(
+      projectBudget,
+      'projectBudget.json',
+      'Record project OprojectObj Failed.'
+    );
+  });
+
+  it('Record project Schedule', async () => {
+    const projectRecord = await ProjectRecord.deployed();
+    console.log('record file projectSchedule.json to contract...');
+    await projectRecord.setProjectSchedule('projectSchedule.json');
+    const projectSchedule = await projectRecord.getProjectSchedule();
+    console.log('ProjectSchedule Doc: ', projectSchedule);
+    assert.equal(
+      projectSchedule,
+      'projectSchedule.json',
+      'Record project projectSchedule Failed.'
+    );
+  });
+
+  it('Record project ROI Document', async () => {
+    const projectRecord = await ProjectRecord.deployed();
+    console.log('record file ProjectROI.json to contract...');
+    await projectRecord.setProjectROI('projectROI.json');
+    const projectROI = await projectRecord.getProjectROI();
+    console.log('projectROI DOC: ', projectROI);
+    assert.equal(
+      projectROI,
+      'projectROI.json',
+      'Record project projectROI Failed.'
+    );
+  });
+
+  it('Record project Manament Plan Document', async () => {
+    const projectRecord = await ProjectRecord.deployed();
+    console.log('record file projectMgmtPlan.json to contract...');
+    await projectRecord.setProjectMgmtPlan('ProjectMgmtPlan.json');
+    const projectMgmtPlan = await projectRecord.getProjectMgmtPlan();
+    console.log('ProjectMgmtPlan DOC: ', projectMgmtPlan);
+    assert.equal(
+      projectMgmtPlan,
+      'ProjectMgmtPlan.json',
+      'Record project ProjectMgmtPlan Failed.'
+    );
+  });
+
+  it('Record project Manament Risk plan document', async () => {
+    const projectRecord = await ProjectRecord.deployed();
+    console.log('record file ProjectRiskPlan.json to contract...');
+    await projectRecord.setProjectRiskPlan('ProjectRiskPlan.json');
+    const projectRiskPlan = await projectRecord.getProjectRiskPlan();
+    console.log('ProjectRiskPlan Doc: ', projectRiskPlan);
+    assert.equal(
+      projectRiskPlan,
+      'ProjectRiskPlan.json',
+      'Record project ProjectRiskPlan Failed.'
+    );
+  });
+
+  it('Record project Risk high', async () => {
+    const projectRecord = await ProjectRecord.deployed();
+    console.log('record risk level high to contract...');
+    await projectRecord.recordRisk('high');
+    const risk = await projectRecord.getRisk();
+    console.log('Risk level: ', risk);
+    assert.equal(risk, 'high', 'Record project Risk level Failed.');
+  });
+
+  it('Record project NPV', async () => {
+    const projectRecord = await ProjectRecord.deployed();
+    console.log('record NPV 1000 to contract...');
+    await projectRecord.recordNPV(1000);
+    const NPV = await projectRecord.getNPV();
+    console.log('NPV: ', NPV);
+    assert.equal(NPV[0], 1000, 'Record project NPV Failed.');
+  });
 });
